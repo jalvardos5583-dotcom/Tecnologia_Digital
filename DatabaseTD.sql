293d33be51e7e109ea24909e964874189c8446c4
JesusAlv <jalvarado5583@gmail.com>
2026-05-21 02:11:19 -0500

    wa

--- DatabaseTD.sql
+++ DatabaseTD.sql
@@ -1,5 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `tecnologia_digital` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `tecnologia_digital`;
+
 -- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
 --
 -- Host: localhost    Database: tecnologia_digital
